cd982eb64c57fb8347ed36f793b912c6a890c9e1
gabrielmendes98 <gabrielmssantiago@gmail.com>
2022-11-28 23:28:55 -0300

    handle api error on employee form

--- src/employee/user-interface/pages/register-employee/views/register-employee.view.tsx
+++ src/employee/user-interface/pages/register-employee/views/register-employee.view.tsx
@@ -9,6 +9,8 @@ import {
 } from 'employee/user-interface/components/alert/alert.component';
 import { Box } from 'employee/user-interface/components/box/box.component';
 import { useState } from 'react';
+import { UnexpectedError } from 'shared/domain/errors/unexpected.error';
+import { EntityValidationError } from 'shared/domain/errors/validation.error';
 import { FormProvider } from 'shared/infra/store/form/form.store';
 import { Form } from '../../../components/form/employee-form.component';
 
@@ -35,12 +37,16 @@ export const RegisterEmployeeMainComponent = ({
         type: 'success',
         message: 'Funcionário cadastrado com sucesso!',
       });
-    } catch (e: any) {
-      console.log(JSON.parse(JSON.stringify(e)));
-      setAlert({
-        type: 'error',
-        message: e.message || 'error message',
-      });
+    } catch (e) {
+      if (e instanceof EntityValidationError) {
+        console.log(JSON.parse(JSON.stringify(e)));
+      }
+      if (e instanceof UnexpectedError) {
+        setAlert({
+          type: 'error',
+          message: e.message,
+        });
+      }
     }
   };
 
