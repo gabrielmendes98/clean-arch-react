644de52999f0d6ada1058067f22d8e9899709179
gabrielmendes98 <gabrielmssantiago@gmail.com>
2022-12-03 01:12:00 -0300

    rename static to static-pages

--- src/shared/user-interface/routes/router.tsx
+++ src/shared/user-interface/routes/router.tsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { MakeRegisterEmployeePage } from 'employee/user-interface/pages/register-employee/register-employee.factory';
-import { MakeHome } from 'static/home/home.factory';
+import { MakeHome } from 'static-pages/home/home.factory';
 import { MakeListEmployeesPage } from 'employee/user-interface/pages/list-employees/list-employees.factory';
 import { PAGES } from 'shared/domain/constants/pages';
 import { MainLayout } from 'shared/user-interface/components/layouts/main/main.layout';
