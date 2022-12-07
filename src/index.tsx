695c22051d56b86f55f4df3e4ab9b6f684ade539
gabrielmendes98 <gabrielmssantiago@gmail.com>
2022-12-07 09:12:56 -0300

    fix imports

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,9 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import { Router } from './shared/user-interface/routes/router';
 import './shared/infra/styles/global.scss';
-import 'react-toastify/dist/ReactToastify.css';
-import { UserProvider } from 'shared/infra/storage/user/user.provider';
+import { UserProvider } from './shared/infra/storage/user/user.provider';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
