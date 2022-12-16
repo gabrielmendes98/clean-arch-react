8d0282f03bde2d4e664001529111f67d0f20bfce
gabrielmendes98 <gabrielmssantiago@gmail.com>
2022-12-16 10:26:30 -0300

    test employee list entity

---
+++ src/employee/domain/entities/__tests__/employee-list.entity.spec.ts
@@ -0,0 +1,47 @@
+import { EmployeeList } from '../employee-list.entity';
+
+const fakeEmployee = {
+  id: 'ce734f82-2fac-4845-b394-66bd67e6e271',
+  document: '03542157015',
+  email: 'some@email.com',
+  name: 'some name',
+  salary: 123123,
+};
+
+const otherFakeEmployee = {
+  id: '654ad5fc-bd1a-41c6-b6ac-297058f7a2a7',
+  document: '39517705034',
+  email: 'fake@email.com',
+  name: 'fake name',
+  salary: 123456,
+};
+
+describe('EmployeeList', () => {
+  it('should start with empty array', () => {
+    const list = new EmployeeList();
+    expect(list.items).toStrictEqual([]);
+  });
+
+  it('should set initial items', () => {
+    const list = new EmployeeList([fakeEmployee]);
+    expect(list.items).toStrictEqual([fakeEmployee]);
+  });
+
+  it('should be able to add item on list head by default', () => {
+    const list = new EmployeeList([fakeEmployee]);
+    list.addItem(otherFakeEmployee);
+    expect(list.items).toStrictEqual([otherFakeEmployee, fakeEmployee]);
+  });
+
+  it('should be able to add item on index', () => {
+    const list = new EmployeeList([fakeEmployee]);
+    list.addItem(otherFakeEmployee, 1);
+    expect(list.items).toStrictEqual([fakeEmployee, otherFakeEmployee]);
+  });
+
+  it('should be able to remove item', () => {
+    const list = new EmployeeList([fakeEmployee]);
+    list.removeItem(fakeEmployee);
+    expect(list.items).toStrictEqual([]);
+  });
+});
