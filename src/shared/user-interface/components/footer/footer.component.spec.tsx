53cb25d870d7fc245f7c3d9061cf52757034e285
Gabriel Santiago <gabrielmssantiago@gmail.com>
2023-01-15 12:54:54 -0300

    test: :rotating_light: remove tests to verify ci status check

--- src/shared/user-interface/components/footer/footer.component.spec.tsx
+++
@@ -1,9 +0,0 @@
-import { render } from 'shared/testing/test-utils';
-import { Footer } from './footer.component';
-
-describe('Footer', () => {
-  it('should match snapshot', () => {
-    const { asFragment } = render(<Footer />);
-    expect(asFragment()).toMatchSnapshot();
-  });
-});
